3cbceaef62fb4db20a0efd6b54d0326fe6bbbadc
Samuel Pascual Argaiz <156894836+tigermax12@users.noreply.github.com>
2026-03-13 18:05:42 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hola():
+    return "¡Bienvenido a BurgerCode! La mejor hamburguesa v1.0"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
