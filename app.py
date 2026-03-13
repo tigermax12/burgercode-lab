27b32d69a7b6ba5e43bf3aced578e45ede7255d4
Samuel Pascual Argaiz <156894836+tigermax12@users.noreply.github.com>
2026-03-13 19:31:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hola():
-    return "¡Bienvenido a BurgerCode! La mejor hamburguesa v1.0"
+    return "¡Bienvenido a BurgerCode! La mejor hamburguesa v2.0"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
