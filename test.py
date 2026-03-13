2f1546732c55c548ac3b93bb278038ca4f96a83b
Samuel Pascual Argaiz <156894836+tigermax12@users.noreply.github.com>
2026-03-13 18:12:07 +0100

    Create  test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+def test_hamburguesa():
+    ingrediente = "carne"
+    assert ingrediente == "carne", "¡Falta la carne en la hamburguesa!"
+    print("Test superado: La hamburguesa tiene carne.")
+
+if __name__ == "__main__":
+    test_hamburguesa()
